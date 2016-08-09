14b1d92bb9b71a608d7267a304630a3f7e32a209
heyyitslillian <lillianliwei@gmail.com>
2016-08-08 22:08:25 -0700

    class exercises

---
+++ foodie 2/js/foodie.js
@@ -0,0 +1,61 @@
+// Elements
+// ------------------------------------
+var form = document.querySelector("form");
+var zip = document.querySelector("form .zip");
+var results = document.querySelector(".results");
+
+
+// Event
+// ------------------------------------
+form.addEventListener('submit', getRestaurants);
+
+
+// Event Handler 
+// ------------------------------------
+function getRestaurants(event) {
+	event.preventDefault();
+
+	var search = zip.value;
+	console.log(search);
+
+	var url = "http://opentable.herokuapp.com/api/restaurants?zip=" + search;
+
+	$.getJSON(url, updateRestaurants);
+}
+
+// Update page
+// ------------------------------------
+function updateRestaurants(json) {
+	console.log('updateRestaurants',json);
+
+	// clears out the old results
+	results.innerHTML = '';
+
+	// add new result for each item in array
+	json.restaurants.forEach(createRestaurant);
+
+var source = document.querySelector("#restaurant-template");
+console.log(source);
+
+
+
+
+
+}
+
+function createRestaurant(restaurant) {
+
+	//still create the li, and pump everything (as a string) into the HTML
+	var li = document.createElement("li");
+
+	var template =
+		'<img src="' + restaurant.image_url + '">' +
+		'<h2>' + restaurant.name + '</h2>' +
+		'<p>' + restaurant.address + '</p>';
+
+	console.log(template);
+	li.innerHTML = template;
+	results.appendChild(li);
+
+}
+
